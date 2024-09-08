2d7d69c49bec87d89a3e54e06e2bffeb4fcb5879
Nour <nouramer725@gmail.com>
2024-09-08 23:05:28 +0300

    initial commit

---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,39 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'HomePage.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        fontFamily: 'Montserrat',
+      ),
+      child: AnimatedSplashScreen(
+        splash: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Lottie.asset(
+                    'assets/images/Animation - 1725817576751.json',
+                    animate: true,
+                    repeat: true,
+                    reverse: true,
+                    fit: BoxFit.cover
+                ),
+              ),
+            ),
+          ],
+        ),
+        nextScreen: const HomePage(),
+        splashIconSize: 600,
+        splashTransition: SplashTransition.scaleTransition,
+        backgroundColor:Colors.white,
+      ),
+    );
+  }
+}
